e66429b5f9755909b1d124cea7e7dc0066951ede
Tony <tonyyu1129@gmail.com>
2024-04-18 21:32:06 -0700

    quote component

--- src/components/quote/Quote.tsx
+++ src/components/quote/Quote.tsx
@@ -8,8 +8,11 @@ type QuoteProps = {
 
 export default function Quote({ quote }: QuoteProps) {
   return (
-    <div className="quote">
-      {typeof quote === "string" ? <span>{quote}</span> : quote}
+    <div className="quote_container">
+      <div className="quote_container__border_left" />
+      <div className="quote_container__text">
+        {typeof quote === "string" ? <span>{quote}</span> : quote}
+      </div>
     </div>
   );
 }
