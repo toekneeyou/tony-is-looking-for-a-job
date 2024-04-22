39649dd5a9b8023be3548dd6ebcde5e1c4821e59
Tony <tonyyu1129@gmail.com>
2024-04-22 16:48:04 -0700

    redid introduction

--- src/sections/introduction/Introduction.tsx
+++ src/sections/introduction/Introduction.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import {
   INTRODUCTION_SECTION_ID,
   INTRODUCTION_SECTION_LEFT_ID,
@@ -6,36 +5,25 @@ import {
   INTRODUCTION_SECTION_Z_INDEX,
 } from "../../App";
 import TonyIceland from "../../assets/tony-iceland.jpg";
-import Quote from "../../components/quote/Quote";
+import TonyIntro from "../../features/tonyIntro/TonyIntro";
 
 import "./introduction.css";
-import TextBody from "../../components/textBody/TextBody";
+import { useEffect } from "react";
 
 export default function Introduction() {
   useEffect(() => {
-    const header = document.querySelector(
-      `#${INTRODUCTION_SECTION_RIGHT_ID} h1`
+    const introductionLeft = document.querySelector(
+      ".introduction_section__left"
     );
-    const quote = document.querySelector(
-      `#${INTRODUCTION_SECTION_RIGHT_ID} .quote_container`
-    );
-    const textBodyBorder = document.querySelector(
-      `#${INTRODUCTION_SECTION_RIGHT_ID} .text_body__border`
-    ) as HTMLElement;
-    const textBodyText = document.querySelector(
-      `#${INTRODUCTION_SECTION_RIGHT_ID} .text_body span`
-    ) as HTMLElement;
-    if (header && quote && textBodyBorder && textBodyText) {
-      header.classList.add("fade-in");
+    const tony = document.querySelector(".tony_intro");
+
+    if (introductionLeft && tony) {
       setTimeout(() => {
-        quote.classList.add("show");
-      }, 500);
+        introductionLeft.classList.add("introduction_section__left__show");
+      }, 250);
       setTimeout(() => {
-        textBodyBorder.style.transform = "translateX(0)";
-        textBodyBorder.style.opacity = "1";
-        textBodyText.style.transform = "translateX(0)";
-        textBodyText.style.opacity = "1";
-      }, 1000);
+        tony.classList.add("tony_intro__show");
+      }, 900);
     }
   }, []);
 
@@ -49,26 +37,14 @@ export default function Introduction() {
         id={INTRODUCTION_SECTION_LEFT_ID}
         className="introduction_section__left"
       >
-        <img src={TonyIceland} />
+        <img src={TonyIceland} className="introduction_section__picture" />
+        <div className="introduction_section__left__overlay" />
       </div>
       <div
         id={INTRODUCTION_SECTION_RIGHT_ID}
         className="introduction_section__right"
       >
-        <div className="introduction_section__text">
-          <div style={{ overflow: "hidden" }}>
-            <h1 className="fade_in">Hi, I&apos;m Tony</h1>
-          </div>
-
-          <Quote quote="Frontend Developer" />
-
-          <TextBody
-            showBorder
-            show={false}
-            text="I have over 4 years of experience building user interfaces with
-              React, a JavaScript library."
-          />
-        </div>
+        <TonyIntro animate />
       </div>
     </section>
   );
