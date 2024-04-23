f1759f7a5d65145030b1e72bb41921d0320f6b5c
Tony <tonyyu1129@gmail.com>
2024-04-23 16:20:23 -0700

    skill ssection done

--- src/sections/skills/Skills.tsx
+++ src/sections/skills/Skills.tsx
@@ -1,19 +1,29 @@
-import { SKILLS_SECTION_ID, SKILLS_SECTION_Z_INDEX } from "../../App";
+import { SKILLS_SECTION_ID, SKILLS_SECTION_Z_INDEX, WHITE } from "../../App";
+import Description from "../../components/description/Description";
+import Quote from "../../components/quote/Quote";
+import TextBody from "../../components/textBody/TextBody";
 import Highlights, { Highlight } from "../../features/highlights/Highlights";
+import SkillsTabs from "../../features/skillsTabs/SkillsTabs";
 
-const skills: Highlight[] = [
-  { main: "4+ years", sub: "Work Experience" },
-  { main: "Frontend", sub: "Web & Mobile" },
-  { main: "UI/UX", sub: "Design & Prototyping" },
-  { main: "Backend", sub: "Databases & APIs" },
-];
+import "./skills.css";
 
 export default function Skills() {
   return (
-    <Highlights
-      highlightsSectionId={SKILLS_SECTION_ID}
-      highlights={skills}
+    <section
+      id={SKILLS_SECTION_ID}
       style={{ zIndex: SKILLS_SECTION_Z_INDEX }}
-    />
+      className="skills_section"
+    >
+      <div className="skills_section__left">
+        <SkillsTabs />
+      </div>
+      <div className="skills_section__right">
+        <Description
+          title="Skills & Tools"
+          body="As a senior software engineer at Nielsen, I've worked with a wide range of frontend tools, with React being my go-to. I've also explored design, backend development, and devops—areas that, while not my main focus, I find both intriguing and rewarding to learn about. I love diving into new technologies and expanding my skill set, keeping my work both dynamic and exciting."
+          position="right"
+        />
+      </div>
+    </section>
   );
 }
