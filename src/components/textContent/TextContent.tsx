05db8b60672aae9175332862e4d573b306fc294c
Tony <tonyyu1129@gmail.com>
2024-04-26 15:41:18 -0700

    delete old components and finish about me section

---
+++ src/components/textContent/TextContent.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from "react";
+import "./textContent.css";
+
+type TextContent = {
+  title: string;
+  bodyText?: string;
+  bodyEl?: ReactNode;
+  button?: ReactNode;
+};
+
+export default function TextContent({
+  title,
+  bodyText,
+  bodyEl,
+  button,
+}: TextContent) {
+  return (
+    <div className="TEXT_CONTENT">
+      <h2 className="josefin_sans_bold">{title}</h2>
+      <div className="divider" />
+      {bodyEl ? bodyEl : <span>{bodyText}</span>}
+      {!!button && button}
+    </div>
+  );
+}
--- src/sections/aboutMe/AboutMe.tsx
+++ src/sections/aboutMe/AboutMe.tsx
@@ -1,25 +1,141 @@
-import {
-  ABOUT_ME_SECTION_ID,
-  ABOUT_ME_SECTION_LEFT_ID,
-  ABOUT_ME_SECTION_RIGHT_ID,
-} from "../../App";
-import Description from "../../components/description/Description";
-import Timeline from "../../features/timeline/Timeline";
+import { ABOUT_ME_SECTION_ID } from "../../constants/id";
+import TextContent from "../../components/textContent/TextContent";
+import uclaLogo from "../../assets/ucla-logo.webp";
+import lewagonLogo from "../../assets/lewagon-logo.jpg";
+import nielsenLogo from "../../assets/nielsen-logo.svg";
+import { ReactNode, useContext, useEffect, useRef, useState } from "react";
+import { combineClasses } from "../../helpers/helpers";
 import "./aboutMe.css";
+import Button from "../../components/button/Button";
+import { AppContext } from "../../App";
 
 export default function AboutMe() {
+  const { sectionIndex } = useContext(AppContext);
+  const [hoveredItem, setHoveredItem] = useState<
+    "ucla" | "lewagon" | "nielsen" | null
+  >(null);
+  const aboutMeRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    const aboutMeEl = aboutMeRef.current as HTMLElement;
+    if (sectionIndex === 1) {
+      aboutMeEl.classList.remove("ABOUT_ME__hide");
+      aboutMeEl.classList.add("ABOUT_ME__show");
+    } else {
+      aboutMeEl.classList.remove("ABOUT_ME__show");
+      aboutMeEl.classList.add("ABOUT_ME__hide");
+    }
+  }, [sectionIndex]);
+
   return (
-    <section id={ABOUT_ME_SECTION_ID} className="about_me">
-      <div id={ABOUT_ME_SECTION_LEFT_ID} className="about_me__left">
-        <Description
+    <section ref={aboutMeRef} id={ABOUT_ME_SECTION_ID} className="ABOUT_ME">
+      <div className="about_me_left">
+        <TextContent
           title="About Me"
-          body="The only thing that excites me more than a beautiful, intuitive user interface is hitting the slopes on my snowboard or sending a fun bouldering route. With over four years of experience in collaborative development environments, I've honed my skills in building engaging applications using React. Though my expertise lies in frontend development, I've been actively involved in every phase of the development lifecycle."
-          position="left"
+          bodyText="The only thing that excites me more than a beautiful, intuitive user interface is hitting the slopes on my snowboard or sending a fun bouldering route. With over four years of experience in collaborative development environments, I've honed my skills in building engaging applications using React. Though my expertise lies in frontend development, I've been actively involved in every phase of the development lifecycle."
+          button={
+            <Button
+              onClick={() => {
+                window.open("https://linkedin.com/in/tonyyu1129", "_blank");
+              }}
+              rightIcon="download"
+            >
+              Download Resume
+            </Button>
+          }
         />
       </div>
-      <div id={ABOUT_ME_SECTION_RIGHT_ID} className="about_me__right">
-        <Timeline />
+      <div className="about_me_right">
+        <div
+          className="about_me_visual"
+          onMouseOut={() => setHoveredItem(null)}
+        >
+          <div className="overlay about_me_visual__overlay" />
+          <AboutMeVisualItem
+            src={nielsenLogo}
+            onMouseOver={() => setHoveredItem("nielsen")}
+            width="55%"
+            isExpanded={hoveredItem === "nielsen"}
+            isCollapsed={hoveredItem !== null && hoveredItem !== "nielsen"}
+            backgroundColor="linear-gradient(45deg, rgba(80,48,170,1) 18%, rgba(48,78,105,1) 50%, rgba(31,44,102,1) 100%)"
+            company="Gracenote, a Nielsen Company"
+            title="Sr. Software Engineer"
+            date="2020 - Present"
+          />
+          <AboutMeVisualItem
+            src={lewagonLogo}
+            onMouseOver={() => setHoveredItem("lewagon")}
+            width="25%"
+            isExpanded={hoveredItem === "lewagon"}
+            isCollapsed={hoveredItem !== null && hoveredItem !== "lewagon"}
+            backgroundColor="#e71005"
+            company="Le Wagon"
+            title="Web Development Program"
+            date="2019"
+          />
+          <AboutMeVisualItem
+            src={uclaLogo}
+            onMouseOver={() => setHoveredItem("ucla")}
+            width="35%"
+            isExpanded={hoveredItem === "ucla"}
+            isCollapsed={hoveredItem !== null && hoveredItem !== "ucla"}
+            backgroundColor="#0f7bb6"
+            company="UCLA Health - Jules Stein Eye Institute"
+            title="Ophthalmic Technician II"
+            date="2016 - 2019"
+          />
+        </div>
       </div>
     </section>
+  );
+}
+
+type AboutMeVisualItemProps = {
+  backgroundColor: string;
+  width: string;
+  onMouseOver: () => void;
+  isExpanded: boolean;
+  isCollapsed: boolean;
+  src: string;
+  title: string;
+  company: string;
+  date: string;
+};
+function AboutMeVisualItem({
+  backgroundColor,
+  width,
+  onMouseOver,
+  isExpanded,
+  isCollapsed,
+  src,
+  title,
+  company,
+  date,
+}: AboutMeVisualItemProps) {
+  return (
+    <div
+      className={combineClasses(["about_me_visual__item"])}
+      onMouseOver={onMouseOver}
+      style={{
+        background: backgroundColor,
+        width: isExpanded ? "100%" : isCollapsed ? "0%" : width,
+      }}
+    >
+      <img src={src} />
+      <div
+        className={combineClasses([
+          "overlay about_me_visual__item__content",
+          isExpanded ? "about_me_visual__item__content__show" : null,
+        ])}
+        style={{
+          opacity: isExpanded ? 1 : 0,
+        }}
+      >
+        <span className="title">{title}</span>
+        <br />
+        <span className="company">{company}</span>
+        <span className="date">{date}</span>
+      </div>
+    </div>
   );
 }
