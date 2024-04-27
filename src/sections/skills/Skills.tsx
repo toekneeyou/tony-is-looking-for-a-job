355de329c9395213b91e57428205ec23b10ea035
Tony <tonyyu1129@gmail.com>
2024-04-26 17:26:11 -0700

    finish skills section

--- src/sections/skills/Skills.tsx
+++ src/sections/skills/Skills.tsx
@@ -1,29 +1,89 @@
-import { SKILLS_SECTION_ID, SKILLS_SECTION_Z_INDEX, WHITE } from "../../App";
-import Description from "../../components/description/Description";
-import Quote from "../../components/quote/Quote";
-import TextBody from "../../components/textBody/TextBody";
-import Highlights, { Highlight } from "../../features/highlights/Highlights";
-import SkillsTabs from "../../features/skillsTabs/SkillsTabs";
-
+import { useContext, useEffect, useRef } from "react";
+import TextContent from "../../components/textContent/TextContent";
+import { Skill, SkillDetail, Tool, skills } from "../../constants/data";
+import { SKILLS_SECTION_ID } from "../../constants/id";
 import "./skills.css";
+import { AppContext } from "../../App";
+import { combineClasses } from "../../helpers/helpers";
 
 export default function Skills() {
+  const skillsRef = useRef<HTMLElement>(null);
+  const { sectionIndex } = useContext(AppContext);
+
+  useEffect(() => {
+    const skillsEl = skillsRef.current as HTMLElement;
+    if (sectionIndex == 2) {
+      skillsEl.classList.add("SKILLS__show");
+      skillsEl.classList.remove("SKILLS__hide");
+    } else {
+      skillsEl.classList.remove("SKILLS__show");
+      skillsEl.classList.add("SKILLS__hide");
+    }
+  }, [sectionIndex]);
   return (
-    <section
-      id={SKILLS_SECTION_ID}
-      style={{ zIndex: SKILLS_SECTION_Z_INDEX }}
-      className="skills_section"
-    >
-      <div className="skills_section__left">
-        <SkillsTabs />
+    <section ref={skillsRef} id={SKILLS_SECTION_ID} className="SKILLS">
+      <div className="skills_left">
+        <div className="skills_visual">
+          {skills.map((s, i) => {
+            return (
+              <SkillItem
+                skill={s}
+                key={s.name}
+                className={`SKILL_ITEM__${i}`}
+              />
+            );
+          })}
+        </div>
       </div>
-      <div className="skills_section__right">
-        <Description
+
+      <div className="skills_right">
+        <TextContent
           title="Skills & Tools"
-          body="As a senior software engineer at Nielsen, I've worked with a wide range of frontend tools, with React being my go-to. I've also explored design, backend development, and devops—areas that, while not my main focus, I find both intriguing and rewarding to learn about. I love diving into new technologies and expanding my skill set, keeping my work both dynamic and exciting."
-          position="right"
+          bodyText="The only thing that excites me more than a beautiful, intuitive user interface is hitting the slopes on my snowboard or sending a fun bouldering route. With over four years of experience in collaborative development environments, I've honed my skills in building engaging applications using React. Though my expertise lies in frontend development, I've been actively involved in every phase of the development lifecycle."
         />
       </div>
     </section>
+  );
+}
+
+type SkillItemProps = {
+  skill: SkillDetail;
+  className: string;
+};
+function SkillItem({ skill, className }: SkillItemProps) {
+  return (
+    <div className={combineClasses(["SKILL_ITEM", className])}>
+      <div
+        className="skill_item__label"
+        style={{
+          transform: "translateX(-48px)",
+        }}
+      >
+        <span>{skill.name}</span>
+      </div>
+
+      <ul
+        className="skill_item__tools"
+        style={{
+          transform: "translateX(-48px)",
+        }}
+      >
+        {skill.tools.map((t) => {
+          return <ToolItem key={t.name} {...t} />;
+        })}
+        <div className="overlay skill_item__overlay" />
+      </ul>
+    </div>
+  );
+}
+
+function ToolItem({ icon, name }: Tool) {
+  return (
+    <li className="TOOL_ITEM">
+      <img src={icon} />
+      <div className="overlay">
+        <span>{name}</span>
+      </div>
+    </li>
   );
 }
