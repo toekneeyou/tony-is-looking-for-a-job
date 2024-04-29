fafc2abf6adf5557579ed15d1ad19a188735b710
Tony <tonyyu1129@gmail.com>
2024-04-29 12:38:15 -0700

    add a mobile versino of Actions

--- src/features/actions/Actions.tsx
+++ src/features/actions/Actions.tsx
@@ -1,24 +1,30 @@
-import { useEffect, useRef } from "react";
+import { useContext, useEffect, useRef } from "react";
 import IconButton from "../../components/iconButton/IconButton";
 import GithubIcon from "../../components/icons/GithubIcon";
 import LinkedInIcon from "../../components/icons/LinkedInIcon";
 import { ACTIONS_ID } from "../../constants/id";
 import { ACTIONS_Z_INDEX } from "../../constants/zIndices";
 import "./actions.css";
+import { AppContext } from "../../App";
+import { combineClasses } from "../../helpers/helpers";
 
 export default function Actions() {
   const actionsRef = useRef<HTMLUListElement>(null);
+  const { isMobile } = useContext(AppContext);
 
   useEffect(() => {
     const aboutMeEl = actionsRef.current as HTMLUListElement;
     aboutMeEl.classList.add("ACTIONS__show");
-  }, []);
+  }, [isMobile]);
 
   return (
     <ul
       ref={actionsRef}
       id={ACTIONS_ID}
-      className="ACTIONS"
+      className={combineClasses([
+        "ACTIONS",
+        isMobile ? "ACTIONS__mobile" : undefined,
+      ])}
       style={{ zIndex: ACTIONS_Z_INDEX }}
     >
       <IconButton
