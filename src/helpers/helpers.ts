155effeb9329b3afb9494589bd97bcd159d598a0
Tony <tonyyu1129@gmail.com>
2024-05-07 22:40:23 -0700

    create the ultimate classNames helper function

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,28 +1,22 @@
 export const classNames = (
-  classStringOrObject: string | { [className: string]: boolean },
-  classObject?: { [className: string]: boolean }
+  classOne: string | string[] | { [className: string]: boolean },
+  classTwo?: string | string[] | { [className: string]: boolean },
+  classThree?: string | string[] | { [className: string]: boolean }
 ) => {
   let classNames: string[] = [];
-  if (typeof classStringOrObject == "string") {
-    classStringOrObject
-      .trim()
-      .split(" ")
-      .forEach((c) => {
-        classNames.push(c.trim());
-      });
-  } else {
-    for (let string in classStringOrObject) {
-      if (classStringOrObject[string]) {
-        classNames.push(string);
+  [classOne, classTwo, classThree].forEach((c) => {
+    if (typeof c === "string") {
+      classNames.push(c.trim());
+    } else if (Array.isArray(c)) {
+      classNames = [...classNames, ...c];
+    } else if (typeof c === "object") {
+      for (let string in c) {
+        if (c[string]) {
+          classNames.push(string.trim());
+        }
       }
     }
-  }
-  if (classObject) {
-    for (let string in classObject) {
-      if (classObject[string]) {
-        classNames.push(string);
-      }
-    }
-  }
+  });
+
   return classNames.join(" ");
 };
